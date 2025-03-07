4fcfc39c474c867765619aeb8d7ccdbae32d0a61
roofers_00_dribble <roofers_00_dribble@icloud.com>
2025-03-07 14:32:07 +0800

    feat: add dropdown menu

--- app/layouts.tsx
+++ app/layouts.tsx
@@ -4,9 +4,17 @@ import {
   MenuUnfoldOutlined,
   UploadOutlined,
   UserOutlined,
-  VideoCameraOutlined
+  VideoCameraOutlined,
 } from '@ant-design/icons'
-import { Button, Layout, Menu, theme } from 'antd'
+import { 
+  Avatar, 
+  Button, 
+  Dropdown,
+  Layout, 
+  Menu, 
+  theme
+ } from 'antd'
+import type { MenuProps } from 'antd';
 import { useShallow } from 'zustand/react/shallow'
 import { useAppStore } from './store'
 
@@ -29,6 +37,25 @@ const Layouts = ({ children }: { children: React.ReactNode }) => {
       toggleCollapsed: state.toggleCollapsed,
     }))
   )
+
+  const items: MenuProps['items'] = [
+    {
+      key: '2',
+      label:'消息中心',
+    },
+    {
+      type: 'divider',
+    },
+    {
+      key: '3',
+      label: '系统设置',
+    },
+    {
+      key: '4',
+      danger: true,
+      label: '退出登录',
+    },
+  ]
 
   /**
    *
@@ -71,17 +98,30 @@ const Layouts = ({ children }: { children: React.ReactNode }) => {
           />
         </Sider>
         <Layout>
-          <Header style={{ padding: 0, background: colorBgContainer }}>
-            <Button
-              type='text'
-              icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
-              onClick={() => handleSideBarChange(!collapsed)}
-              style={{
-                fontSize: '16px',
-                width: 64,
-                height: 64
-              }}
-            />
+          <Header 
+            className='flex justify-between'
+            style={{ padding: 0, background: colorBgContainer }}
+          >
+            <div>
+              <Button
+                type='text'
+                icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+                onClick={() => handleSideBarChange(!collapsed)}
+                style={{
+                  fontSize: '16px',
+                  width: 64,
+                  height: 64
+                }}
+              />
+            </div>
+            <div className='px-8'>
+              <Dropdown menu={{ items }}>
+                <div className='flex items-center justify-center'>
+                  <Avatar />
+                  <div className='pl-4'>Username</div>
+                </div>
+              </Dropdown>
+            </div>
           </Header>
           <Content
             style={{
